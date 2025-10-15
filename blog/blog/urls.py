bf2606687be1c169edd1aae59b3d027a465359dd
Axeniter <anamievoid@gmail.com>
2025-10-15 10:14:34 +1000

    Add: views for articles and registration system
    add view for article list
    add view for article detail
    add view for article create and edit
    add view for article delete
    add view for custom logout
    add view for register
    modify urls
    modify settings with new login urls

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,23 +1,22 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from news_blog import views
 
 urlpatterns = [
-    path("", views.home),
-    path("about/", views.about),
-    path("contact/", views.contact),
-    path("feedback/", views.feedback),
+    path("", views.home, name='home'),
+    path("about/", views.about, name='about'),
+    path("contact/", views.contact, name='contact'),
+    path("feedback/", views.feedback, name='feedback'),
 
-    path("create-article/", views.create_article),
-    path("edit-article/", views.edit_article),
-    path("delete-article/", views.delete_article),
+    path('create-article/', views.create_article, name='create_article'),
+    path('edit-article/<int:article_id>/', views.edit_article, name='edit_article'),
+    path('delete-article/<int:article_id>/', views.delete_article, name='delete_article'),
 
-    path("articles/", views.all_articles),
-    path("articles/<str:category>/", views.articles_by_category),
-    path("articles/<int:id>/", views.article_detail),
+    path('articles/', views.articles_list, name='articles_list'),
+    path('articles/<str:category>/', views.articles_list, name='articles_by_category'),
+    path('article/<int:article_id>/', views.article_detail, name='article_detail'),
 
-    path('register/', views.register),
-    path('login/', views.custom_login),
-    path('logout/', views.custom_logout),
-
-    path("news/<int:id>", views.article)
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', views.custom_logout, name='logout'),
 ]
--- blog/news_blog/views.py
+++ blog/news_blog/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-from .forms import FeedbackForm
+from django.shortcuts import render, redirect
+from . import forms
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.db.models import Q
+from .models import Article
+from .forms import ArticleForm, CommentForm, CustomUserRegistrationForm
+from django.contrib.auth import logout
 
 def home(request):
     return render(request, "home.html")
@@ -12,7 +19,7 @@ def contact(request):
 
 def feedback(request):
     if request.method == 'POST':
-        form = FeedbackForm(request.POST)
+        form = forms.FeedbackForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
@@ -25,8 +32,117 @@ def feedback(request):
             })
         return render(request, "feedback.html", {"form": form})
     else:
-        form = FeedbackForm()
+        form = forms.FeedbackForm()
         return render(request, "feedback.html", {"form": form})
     
-def article(request, id):
-    return render(request, "article.html", {"id": id})
+def articles_list(request, category=None):
+    articles = Article.objects.all()
+    
+    if category:
+        valid_categories = [choice[0] for choice in Article.CATEGORY_CHOICES]
+        if category not in valid_categories:
+            return render(request, 'articles_invalid_category.html', {'category': category})
+        articles = articles.filter(category=category)
+    
+    search_query = request.GET.get('search', '')
+    if search_query:
+        articles = articles.filter(
+            Q(title__icontains=search_query) | 
+            Q(text__icontains=search_query)
+        )
+    
+    context = {
+        'articles': articles,
+        'category': category,
+        'search_query': search_query,
+        'category_choices': Article.CATEGORY_CHOICES,
+    }
+    return render(request, 'articles_list.html', context)
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    comments = article.comments.all()
+    
+    if request.method == 'POST':
+        if not request.user.is_authenticated:
+            return redirect('login')
+        
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.article = article
+            comment.author = request.user
+            comment.save()
+            return redirect('article_detail', article_id=article.id)
+    else:
+        form = CommentForm()
+    
+    context = {
+        'article': article,
+        'comments': comments,
+        'form': form,
+    }
+    return render(request, 'article_detail.html', context)
+
+@login_required
+def create_article(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('articles_list')
+    else:
+        form = ArticleForm()
+    
+    context = {'form': form}
+    return render(request, 'article_create.html', context)
+
+@login_required
+def edit_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    
+    if article.author != request.user:
+        return redirect('articles_list')
+    
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('article_detail', article_id=article.id)
+    else:
+        form = ArticleForm(instance=article)
+    
+    context = {'form': form, 'article': article}
+    return render(request, 'article_create.html', context)
+
+@login_required
+def delete_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    
+    if article.author != request.user:
+        return redirect('articles_list')
+    
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles_list')
+    
+    context = {'article': article}
+    return render(request, 'article_delete.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def custom_logout(request):
+    logout(request)
+    return redirect('home')
